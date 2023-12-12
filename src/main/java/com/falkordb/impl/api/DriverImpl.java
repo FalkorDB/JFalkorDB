2280b32586c20786e11275879890090f90bfee44
Guy Korland <gkorland@gmail.com>
2023-12-12 12:43:14 +0200

    add more factories

--- src/main/java/com/falkordb/impl/api/DriverImpl.java
+++ src/main/java/com/falkordb/impl/api/DriverImpl.java
@@ -1,5 +1,7 @@
 package com.falkordb.impl.api;
 
+import java.net.URI;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.util.Pool;
@@ -9,20 +11,34 @@ public class DriverImpl implements com.falkordb.Driver {
     private final Pool<Jedis> pool;
 
     /**
-     * Creates a client running on the local machine
+     * Creates a client running on the specific host/post
+     *
+     * @param host Server host
+     * @param port Server port
      */
-    public DriverImpl() {
-        this("localhost", 6379);
+    public DriverImpl(String host, int port) {
+        this.pool = new JedisPool(host, port);
     }
 
     /**
      * Creates a client running on the specific host/post
      *
-     * @param host Redis host
-     * @param port Redis port
+     * @param host Server host
+     * @param port Server port
+     * @param user username
+     * @param password password
      */
-    public DriverImpl(String host, int port) {
-        this.pool = new JedisPool(host, port);
+    public DriverImpl(String host, int port, String user, final String password) {
+        this.pool = new JedisPool(host, port, user, password);
+    }
+
+    /**
+     * Creates a client using the specific uri
+     *
+     * @param uri server uri
+     */
+    public DriverImpl(URI uri) {
+        this.pool = new JedisPool(uri);
     }
 
     @Override
