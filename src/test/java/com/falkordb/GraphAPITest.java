e991a189e69a36d810e3f5dbc8c1e388b258ca32
Barak Bar Orion <barak.bar@gmail.com>
2024-10-10 13:08:44 +0300

    Add sdk init file Fix testVecf32

--- src/test/java/com/falkordb/GraphAPITest.java
+++ src/test/java/com/falkordb/GraphAPITest.java
@@ -803,7 +803,12 @@ public class GraphAPITest {
         ResultSet resultSet = client.query("RETURN vecf32([2.1, -0.82, 1.3, 4.5]) AS vector");
         Assert.assertEquals(1, resultSet.size());
         Record r = resultSet.iterator().next();
-        Assert.assertEquals(Arrays.asList(2.1f, -0.82f, 1.3f, 4.5f), r.getValue(0));   
+        List<Double> vector = r.getValue(0);
+        Assert.assertEquals(4, vector.size());
+        Assert.assertEquals(2.1f, vector.get(0), 0.01);
+        Assert.assertEquals(-0.82f, vector.get(1), 0.01);
+        Assert.assertEquals(1.3f, vector.get(2), 0.01);
+        Assert.assertEquals(4.5f, vector.get(3), 0.01);
     }
 
     @Test
