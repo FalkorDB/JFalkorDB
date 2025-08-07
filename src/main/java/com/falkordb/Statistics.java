c1a7816c9a9b46b121d32c34c5a5e472f8942b26
Guy Korland <gkorland@gmail.com>
2025-08-07 15:11:42 +0300

    add missing jdocs (#155)
    * add missing jdocs
    
    * revert changes

--- src/main/java/com/falkordb/Statistics.java
+++ src/main/java/com/falkordb/Statistics.java
@@ -1,21 +1,54 @@
 package com.falkordb;
 
 
+/**
+ * An interface for the statistics of a result set.
+ */
 public interface Statistics {
 	
 	/**
 	 * Different Statistics labels 
 	 */
 	enum Label{
+		/**
+		 * The number of labels added.
+		 */
 		LABELS_ADDED("Labels added"),
+		/**
+		 * The number of indices created.
+		 */
 		INDICES_ADDED("Indices created"),
+		/**
+		 * The number of indices deleted.
+		 */
 		INDICES_DELETED("Indices deleted"),
+		/**
+		 * The number of nodes created.
+		 */
 		NODES_CREATED("Nodes created"),
+		/**
+		 * The number of nodes deleted.
+		 */
 		NODES_DELETED("Nodes deleted"),
+		/**
+		 * The number of relationships deleted.
+		 */
 		RELATIONSHIPS_DELETED("Relationships deleted"),
+		/**
+		 * The number of properties set.
+		 */
 		PROPERTIES_SET("Properties set"),
+		/**
+		 * The number of relationships created.
+		 */
 		RELATIONSHIPS_CREATED("Relationships created"),
+		/**
+		 * Whether the execution was cached.
+		 */
 		CACHED_EXECUTION("Cached execution"),
+		/**
+		 * The query internal execution time.
+		 */
 		QUERY_INTERNAL_EXECUTION_TIME("Query internal execution time");
 
 	    private final String text;
@@ -51,21 +84,48 @@ public interface Statistics {
 	 */
 	String getStringValue(Statistics.Label label);
 
+	/**
+	 * @return the number of nodes created
+	 */
 	int nodesCreated();
 	
+	/**
+	 * @return the number of nodes deleted
+	 */
 	int nodesDeleted();
 	
+	/**
+	 * @return the number of indices added
+	 */
 	int indicesAdded();
 
+	/**
+	 * @return the number of indices deleted
+	 */
 	int indicesDeleted();
 	
+	/**
+	 * @return the number of labels added
+	 */
 	int labelsAdded();
 	
+	/**
+	 * @return the number of relationships deleted
+	 */
 	int relationshipsDeleted();
 	
+	/**
+	 * @return the number of relationships created
+	 */
 	int relationshipsCreated();
 	
+	/**
+	 * @return the number of properties set
+	 */
 	int propertiesSet();
 
+	/**
+	 * @return whether the execution was cached
+	 */
 	boolean cachedExecution();
 }
