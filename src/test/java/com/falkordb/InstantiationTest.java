418f669588121206dcbfabca8fc5fbd2164541f5
drr00t <adribeiro@gmail.com>
2025-08-11 22:00:25 -0300

    testcontainer integration

--- src/test/java/com/falkordb/InstantiationTest.java
+++ src/test/java/com/falkordb/InstantiationTest.java
@@ -2,15 +2,17 @@ package com.falkordb;
 
 import java.net.URI;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.falkordb.test.BaseTestIT;
+import com.redis.testcontainers.RedisContainer;
+import org.junit.jupiter.api.*;
+import org.testcontainers.junit.jupiter.Container;
 
-public class InstantiationTest {
+public class InstantiationTest extends BaseTestIT {
     private GraphContextGenerator client;
 
     @Test
     public void createDefaultClient() {
+
         client = FalkorDB.driver().graph("g");
         ResultSet resultSet = client.query("CREATE ({name:'bsb'})");
         Assertions.assertEquals(1, resultSet.getStatistics().nodesCreated());
@@ -18,21 +20,22 @@ public class InstantiationTest {
 
     @Test
     public void createClientWithHostAndPort() {
-        client = FalkorDB.driver("localhost", 6379).graph("g");
+        client = FalkorDB.driver(getFalkordbHost(), getFalkordbPort()).graph("g");
         ResultSet resultSet = client.query("CREATE ({name:'bsb'})");
         Assertions.assertEquals(1, resultSet.getStatistics().nodesCreated());
     }
 
     @Test
     public void createClientWithHostAndPortNoUser() {
-        client = FalkorDB.driver("localhost", 6379, null, null).graph("g");
+        client = FalkorDB.driver(getFalkordbHost(), getFalkordbPort(), null, null).graph("g");
         ResultSet resultSet = client.query("CREATE ({name:'bsb'})");
         Assertions.assertEquals(1, resultSet.getStatistics().nodesCreated());
     }
 
     @Test
     public void createClientWithURL() {
-        client = FalkorDB.driver(URI.create("redis://localhost:6379")).graph("g");
+        client = FalkorDB.driver(URI.create(String.format("redis://%s:%d",
+                getFalkordbHost(), getFalkordbPort()))).graph("g");
         ResultSet resultSet = client.query("CREATE ({name:'bsb'})");
         Assertions.assertEquals(1, resultSet.getStatistics().nodesCreated());
     }
---
+++ src/test/java/com/falkordb/test/BaseTestIT.java
@@ -0,0 +1,42 @@
+package com.falkordb.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+public class BaseTestIT {
+    private static final Logger log = LoggerFactory.getLogger(BaseTestIT.class);
+    public static final DockerImageName FALOKRDB_IMAGE = DockerImageName.parse("falkordb/falkordb:latest");
+
+    @Container
+    protected static GenericContainer<?> continerFalkordb;
+    private static int falkordbPort = 6379; // Default port for Falkordb, adjust if necessary
+
+    @BeforeAll
+    public static void setUpContainer() {
+        continerFalkordb = new GenericContainer<>(FALOKRDB_IMAGE)
+                .withExposedPorts(falkordbPort)
+                .withLogConsumer(new Slf4jLogConsumer(log)); // Replace 6379 with Falkordb's default port if different
+        continerFalkordb.start();
+    }
+
+    @AfterAll
+    public static void tearDownContainer() {
+        if (continerFalkordb != null) {
+            continerFalkordb.stop();
+        }
+    }
+
+    protected int getFalkordbPort() {
+        return continerFalkordb.getFirstMappedPort();
+    }
+
+    protected String getFalkordbHost() {
+        return continerFalkordb.getHost();
+    }
+}
